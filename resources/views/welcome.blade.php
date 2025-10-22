ec419c63c1d3f6b45bf1dfaa1f0bbce46274e70e
Sahrul Adicandra <137270991+addaan1@users.noreply.github.com>
2025-10-22 10:24:17 +0700

    tes lagi (#2)
    * ganti komen
    
    * ganti lagi
    
    * tes
    
    ---------
    
    Co-authored-by: adi <adieeffendy5@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,14 +11,14 @@
             Kami bantu Anda menemukan pembeli yang tepat dan mengelola transaksi dengan mudah, sehingga Anda bisa fokus pada panen.
         </p>
             
-            <!-- Tombol -->
+            <!-- Tombol-->
             <div class="d-flex">
                 <a href="{{ route('register') }}" class="btn btn-success btn-lg me-3" style="background-color: #4CAF50; border-color: #4CAF50; border-radius: 20px; padding: 10px 30px;">Daftar Sekarang!</a>
                 <a href="{{ route('how-it-works') }}" class="btn btn-warning btn-lg" style="background-color: #FFC107; border-color: #FFC107; color: #333; border-radius: 20px; padding: 10px 30px;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill me-2" viewBox="0 0 16 16">
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"/>
                     </svg>
-                    How It Works yaa
+                    How It Works mantap
                 </a>
             </div>
         </div>
